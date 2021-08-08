47dad0861952e732c4ca949212abc80541f656cf
Javiiwind <82738818+Javiiwind@users.noreply.github.com>
2021-08-08 18:14:05 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 function Boton(){
     let resultado = document.querySelector("input").value;
-    let total = (Math.round((resultado * 1.64) * 100)/100); 
-    console.log(`El precio total de la compra en steam es: $${total}`);
+    let total = (Math.round((resultado * 1.64) * 100)/100);
     document.getElementById("total").innerHTML = `<div class="row border border-primary justify-content-center">
     $${total}
     </div>`;
