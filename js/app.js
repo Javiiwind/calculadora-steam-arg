173a20fcdac3c0208ba79ec00111ed69a802b9bb
Javiiwind <82738818+Javiiwind@users.noreply.github.com>
2021-08-15 15:21:14 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function Boton(){
     let resultado = document.querySelector("input").value;
-    let total = (Math.round((resultado * 1.64) * 100)/100);
+    let total = (Math.round((resultado * 1.65) * 100)/100);
     document.getElementById("total").innerHTML = `<div class="row border border-primary justify-content-center">
     $${total}
     </div>`;
